76d60371d3ccc3145754c899d9484790361d67b9
VaibhavAna <vaibhavanand2494@gmail.com>
2026-06-02 20:06:42 +0530

    code1

--- Interview/JS/code1.js
+++ Interview/JS/code1.js
@@ -1,8 +1,13 @@
 //loops
 
-//for loop :-Used when you know how amy times you wan to repeat.
+//for loop :-Used when you know how many times you want to repeat.
 
 // for (let i=1; i<=5 ; i++){
+//     console.log(i);
+// }
+
+
+// for (let i=1;i<=10;i++){
 //     console.log(i);
 // }
 
@@ -16,6 +21,14 @@
 //     i++;
 // }
 
+// let i =1;
+
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+
 //do while:-Execute the code at least once 
 
 // let i= 1;
@@ -24,6 +37,14 @@
 //     console.log(i);
 //     i++;
 // }while (i<=3);
+
+
+let i =1;
+
+do{
+    console.log(i);
+    i++;
+}while (i<=10);
 
 
 //for of loop:-Used to loop through arrays and other iterable objects.
@@ -40,3 +61,64 @@
 //     console.log(i);
 // }
 
+//Print only even numbers from 1 to 20.
+
+// for (let i =1;i<=20; i++){
+//     if (i %2 === 0 ){
+//         console.log(i);
+//     }
+// }
+//Short way 
+
+// for (let i=2;i<=10;i+=2){
+//     console.log(i);
+// }
+
+
+//Print only odd numbers from 1 to 20.
+
+// for (let i=1;i<=10;i++){
+//     if(i %2 !==0){
+//         console.log(i);
+//     }
+// }
+
+//Short method
+
+// for(let i=1;i<=20;i+=2){
+//     console.log(i);
+// }
+
+//Challenge: Can you write a loop that prints the odd numbers from 20 down to 1?
+
+// for (let i = 19; i >= 1; i -= 2) {
+//     console.log(i);
+// }
+
+//Calculate the sum of numbers from 1 to 100.
+
+// let sum =0;
+// for (let i=1; i<=100; i++){
+//     sum+=i;
+// }
+// console.log(sum);
+
+//Print numbers from 100 down to 1.
+
+// for (let i=100;i>=1;i--){
+//     console.log(i);
+// }
+
+//Print the multiplication table of any number.
+
+// let number =7;
+
+// for (let i=1;i<=10; i++){
+//     console.log(`${number}*${i}=${number *1}`);
+// }
+
+// let numner =15;
+
+// for (let i =1;i<=10;i++){
+//     console.log(`${number}*${i}=${num *1}`);
+// }
---
+++ Interview/JS/code2.js
@@ -0,0 +1,32 @@
+//Print number 1 to 100
+
+
+// for (let i=1;i<=100;i++){
+//     console.log(i);
+// }
+
+//Print even numbers from 1 to 100.
+
+// for (let i=1;i<=100;i+=2){
+//     console.log(i);
+// }
+
+//Print odd numbers from 1 to 100.
+
+//  for (let i=1;i<=100;i++){
+//     if(i %2 !==0){
+//        console.log(i);
+//    }
+// }
+
+//Calculate the sum of numbers from 1 to 100.
+
+let sum=0;
+
+for (let i=1;i<=100;i++){
+        sum+=i;
+}
+console.log(i);
+
+  
+
