17c4a020d93b9de7dc7e50f1b0275e5d89c02cdd
VaibhavAna <vaibhavanand2494@gmail.com>
2026-06-02 23:25:45 +0530

    hi

--- Interview/JS/code2.js
+++ Interview/JS/code2.js
@@ -21,12 +21,12 @@
 
 //Calculate the sum of numbers from 1 to 100.
 
-let sum=0;
+// let sum=0;
 
-for (let i=1;i<=100;i++){
-        sum+=i;
-}
-console.log(i);
+// for (let i=1;i<=100;i++){
+//         sum+=i;
+// }
+// console.log(i);
 
   
 
